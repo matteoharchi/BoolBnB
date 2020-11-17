8701d516a3e64cde30a142325876ab43ec5d80b0
Michele Prova <michele.prova99@gmail.com>
2020-11-17 18:11:56 +0100

    Creazione layout secondario per tutte le altre pagine (tranne la home).

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.authlayout')
 @section('content')
 <div class="container">
     @if ($errors->any())
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.authlayout')
 @section('content')
 <div class="container">
     <div class="row" id="app">
