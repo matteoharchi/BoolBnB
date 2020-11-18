e815943b0344bf83c15a0eadb1ee4d1d657232be
Michele Prova <michele.prova99@gmail.com>
2020-11-18 12:21:52 +0100

    footer fissato(da sistemare)

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 @section('content')
-  <div class="container">
-      <input type="search" id="search" class="form-control" autocomplete="off" placeholder="Dove vuoi andare?">
-  </div>
+  
+
 @endsection
