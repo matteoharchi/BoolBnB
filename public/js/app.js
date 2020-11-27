5b07888fc5919c5c9c50dffca74df1498b433b98
FrancescoLunesu <francesco.lunesu@outlook.it>
2020-11-27 12:28:03 +0100

    creazione head comune nei layout modifiche grafiche focus textarea

--- public/js/app.js
+++ public/js/app.js
@@ -51646,8 +51646,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\mamp_public\boolBnB\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\mamp_public\boolBnB\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP_PUBLIC\Progetto Finale\BoolBnB\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\MAMP_PUBLIC\Progetto Finale\BoolBnB\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
